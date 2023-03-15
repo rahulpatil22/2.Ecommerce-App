70f75243c551e4761019ae92b191479d36abbbe1
Rahul Gopal Patil <rahulil@cybage.com>
2023-03-15 18:56:34 +0530

    Json Data

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Records from "../data.json"
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
